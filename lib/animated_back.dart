be19884bf23cbe3357139cd0c35694228cd62fa7
Saheb Singh Tuteja <sahebsingh.tuteja.sa@gmail.com>
2019-12-04 16:44:31 +0530

    Flare animation added

--- lib/animated_back.dart
+++ lib/animated_back.dart
@@ -8,7 +8,7 @@ class AnimatedBackground extends StatelessWidget {
       Track("color1").add(Duration(seconds: 3),
           ColorTween(begin: Color(0xffD38312), end: Colors.lightBlue.shade900)),
       Track("color2").add(Duration(seconds: 3),
-          ColorTween(begin: Color(0xffA83279), end: Colors.blue.shade600))
+          ColorTween(begin: Color(0xffA83279), end: Colors.blue.shade600)),
     ]);
 
     return ControlledAnimation(
--- lib/digital_clock.dart
+++ lib/digital_clock.dart
@@ -3,11 +3,13 @@
 // found in the LICENSE file.
 
 import 'dart:async';
+import 'package:flare_flutter/flare_actor.dart';
 import 'package:flutter_clock_helper/model.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/intl.dart';
 import './gradient_back.dart';
 import './animated_back.dart';
+import 'package:flare_dart/actor.dart';
 
 enum _Element {
   background,
@@ -107,11 +109,11 @@ class _DigitalClockState extends State<DigitalClock> {
     final minute = DateFormat('mm').format(_dateTime);
     final seconds = DateFormat('ss').format(_dateTime);
     final fontSize = MediaQuery.of(context).size.width / 6; // Initial : 3.5
-    final offset = -fontSize / 7;
+    // final offset = -fontSize / 7;
     final date = DateFormat('yMMMMd').format(_dateTime);
     final defaultStyle = TextStyle(
       color: colors[_Element.text],
-      fontFamily: 'Monoton',
+      fontFamily: 'Tomorrow',
       fontSize: fontSize,
       shadows: [
         Shadow(
@@ -131,28 +133,51 @@ class _DigitalClockState extends State<DigitalClock> {
             // color: colors[_Element.background],
             child: Stack(
               children: <Widget>[
-                Positioned.fill(child: AnimatedBackground()),
-                AnimatedWave(
-                  height: MediaQuery.of(context).size.height,
-                  speed: 1.0,
-                ),
-                Center(
+                Positioned.fill(
+                    child: FlareActor(
+                  "assets/Morning.flr",
+                  animation: 'Sunrise',
+                  fit: BoxFit.fitWidth,
+                )),
+                // Positioned.fill(child: AnimatedBackground()),
+                // AnimatedWave(
+                //   height: MediaQuery.of(context).size.height,
+                //   speed: 1.0,
+                // ),
+                new Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
                   child: Row(
                     mainAxisAlignment: MainAxisAlignment.spaceEvenly,
                     children: <Widget>[
                       Text(hour),
-                      Text(minute),
-                      Text(seconds)
+                      Container(
+                        height: MediaQuery.of(context).size.height * 0.2,
+                        width: MediaQuery.of(context).size.width * 0.1,
+                        // color: Colors.red,
+                        child: Center(
+                          child: FlareActor(
+                            "assets/Blinker.flr",
+                            animation: 'Blink',
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      ),
+                      Text(minute)
                     ],
                   ),
                 ),
-                Center(
-                    child: Padding(
-                      padding: EdgeInsets.only(top: MediaQuery.of(context).size.height/1.8),
-                      child: Text(date,style: TextStyle(fontSize: 30,fontFamily: 'Press'),
-                    ),
-                  ),
-                ),
+                // Center(
+                //   child: Padding(
+                //     padding: EdgeInsets.only(
+                //         bottom: MediaQuery.of(context).size.height / 7),
+                //     child: Text(
+                //       date,
+                //       style: TextStyle(fontSize: 30, fontFamily: 'Press',shadows: []),
+                //     ),
+                //   ),
+                // ),
               ],
             ),
           ),
